4f0d6d772bae6fc702ace5b95b84df3015dc0433
BG Corp <cleverdmasoy@gmail.com>
2023-11-21 10:16:21 +0800

    first commit

---
+++ ReportServer/Services/CustomReportStorageWebExtension.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DevExpress.XtraReports.UI;
+using ReportServer.PredefinedReports;
+using ReportServer.Data;
+using DevExpress.XtraReports;
+using DevExpress.XtraReports.Web.Extensions;
+
+namespace ReportServer.Services
+{
+    public class CustomReportStorageWebExtension : ReportStorageWebExtension
+    {
+
+        public Dictionary<string, XtraReport> Reports = new Dictionary<string, XtraReport>();
+        protected ReportDbContext DbContext { get; set; }
+        public CustomReportStorageWebExtension(ReportDbContext dbContext) {
+            this.DbContext = dbContext;
+
+            string reportDirectory = "C:\\Users\\bgcor\\OneDrive\\Desktop\\ReportDir\\";
+
+            string[] files = Directory.GetFiles(reportDirectory);
+
+            foreach(var file in files)
+            {
+                string url = Path.GetFileNameWithoutExtension(file);
+                XtraReport report = XtraReport.FromFile(file, true);
+
+                Reports.Add(url, report);
+            }
+        }
+
+        
+
+        public override bool CanSetData(string url) {
+            // Determines whether a report with the specified URL can be saved.
+            // Add custom logic that returns **false** for reports that should be read-only.
+            // Return **true** if no valdation is required.
+            // This method is called only for valid URLs (if the **IsValidUrl** method returns **true**).
+
+            return true;
+        }
+
+        public override bool IsValidUrl(string url) {
+            // Determines whether the URL passed to the current report storage is valid.
+            // Implement your own logic to prohibit URLs that contain spaces or other specific characters.
+            // Return **true** if no validation is required.
+
+            return true;
+        }
+
+        public override byte[] GetData(string url)
+        {
+            //var parts = url.Split('?');
+            //var reportName = parts[0];
+            //
+            //Debug.WriteLine("BusinessCode: " + url);
+
+            if (Reports.TryGetValue(url, out XtraReport report))
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+
+                    report.RequestParameters = false;
+                    report.SaveLayoutToXml(stream);
+                    return stream.ToArray();
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override Dictionary<string, string> GetUrls()
+        {
+            return Reports.ToDictionary(x => x.Key, y => y.Key);
+        }
+
+        public override void SetData(XtraReport report, string url)
+        {
+            if (Reports.ContainsKey(url))
+            {
+                Reports[url] = report;
+            }
+            else
+            {
+                Reports.Add(url, report);
+            }
+
+            using (FileStream stream = new FileStream("C:\\Users\\bgcor\\OneDrive\\Desktop\\ReportDir\\" + url + ".resx", FileMode.Create))
+            {
+                report.SaveLayoutToXml(stream);
+            }
+        }
+
+        public override string SetNewData(XtraReport report, string defaultUrl)
+        {
+            SetData(report, defaultUrl);
+
+            using (FileStream stream = new FileStream("C:\\Users\\bgcor\\OneDrive\\Desktop\\ReportDir\\" + defaultUrl + ".resx", FileMode.Create))
+            {
+                report.SaveLayoutToXml(stream);
+            }
+
+            return defaultUrl;
+        }
+    }
+}
---
+++ ReportServer/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using DevExpress.AspNetCore;
+using DevExpress.AspNetCore.Reporting;
+using DevExpress.Security.Resources;
+using DevExpress.XtraReports.Web.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ReportServer.Services;
+using ReportServer.Data;
+
+namespace ReportServer {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddDevExpressControls();
+            services.AddScoped<ReportStorageWebExtension, CustomReportStorageWebExtension>();
+            services
+        .AddCors(options => {
+            options.AddPolicy("CorsPolicy", builder => {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.WithHeaders("Content-Type");
+            });
+        });
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services
+                .AddMvc()
+                .AddNewtonsoftJson();
+            services.ConfigureReportingServices(configurator => {
+                configurator.ConfigureReportDesigner(designerConfigurator => {
+                    designerConfigurator.RegisterDataSourceWizardConnectionStringsProvider<CustomSqlDataSourceWizardConnectionStringsProvider>();
+                    designerConfigurator.RegisterDataSourceWizardJsonConnectionStorage<CustomDataSourceWizardJsonDataConnectionStorage>(true);
+                });
+                configurator.ConfigureWebDocumentViewer(viewerConfigurator => {
+                    viewerConfigurator.UseCachedReportSourceBuilder();
+                    viewerConfigurator.RegisterJsonDataConnectionProviderFactory<CustomJsonDataConnectionProviderFactory>();
+                    viewerConfigurator.RegisterConnectionProviderFactory<CustomSqlDataConnectionProviderFactory>();
+                });
+                configurator.UseAsyncEngine();
+            });
+            services.AddDbContext<ReportDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("ReportsDataConnectionString")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,  ReportDbContext db) {
+            db.InitializeDatabase();
+            
+            var contentDirectoryAllowRule = DirectoryAccessRule.Allow(new DirectoryInfo(Path.Combine(env.ContentRootPath, "..", "Content")).FullName);
+            AccessSettings.ReportingSpecificResources.TrySetRules(contentDirectoryAllowRule, UrlAccessRule.Allow());
+            DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions;
+            app.UseDevExpressControls();
+
+            System.Net.ServicePointManager.SecurityProtocol |= System.Net.SecurityProtocolType.Tls12;
+            if(env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
